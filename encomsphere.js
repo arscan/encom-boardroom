4e5a97158a2f4f92bfc712f7966025b7a686604f
Rob Scanlon <robscanlon@gmail.com>
2013-12-25 13:54:45 -0500

    added a textureanimator

--- encomsphere.js
+++ encomsphere.js
@@ -24,6 +24,45 @@
         }
     };
 
+    // from http://stemkoski.github.io/Three.js/Texture-Animation.html
+    var TextureAnimator = function(texture, tilesHoriz, tilesVert, numTiles, tileDispDuration) 
+    {   
+        // note: texture passed by reference, will be updated by the update function.
+
+        this.tilesHorizontal = tilesHoriz;
+        this.tilesVertical = tilesVert;
+        // how many images does this spritesheet contain?
+        //  usually equals tilesHoriz * tilesVert, but not necessarily,
+        //  if there at blank tiles at the bottom of the spritesheet. 
+        this.numberOfTiles = numTiles;
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping; 
+        texture.repeat.set( 1 / this.tilesHorizontal, 1 / this.tilesVertical );
+
+        // how long should each image be displayed?
+        this.tileDisplayDuration = tileDispDuration;
+
+        // how long has the current image been displayed?
+        this.currentDisplayTime = 0;
+
+        // which image is currently being displayed?
+        this.currentTile = 0;
+
+        this.update = function( milliSec )
+        {
+            this.currentDisplayTime += milliSec;
+            while (this.currentDisplayTime > this.tileDisplayDuration)
+                {
+                    this.currentDisplayTime -= this.tileDisplayDuration;
+                    this.currentTile++;
+                    if (this.currentTile == this.numberOfTiles)
+                        this.currentTile = 0;
+                    var currentColumn = this.currentTile % this.tilesHorizontal;
+                    texture.offset.x = currentColumn / this.tilesHorizontal;
+                    var currentRow = Math.floor( this.currentTile / this.tilesHorizontal );
+                    texture.offset.y = currentRow / this.tilesVertical;
+                }
+        };
+    }   
 
     /* globe */
 

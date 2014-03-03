5705cfd665aa789487e0a3af5e69ad5ef86486eb
Rob Scanlon <robscanlon@gmail.com>
2014-03-03 18:56:29 -0500

    animate content boxes

--- light-table-scripts.js
+++ light-table-scripts.js
@@ -154,6 +154,77 @@ $(function(){
 
     };
 
+    var animateContentBoxes = function(item, extraDelay) {
+
+        var itemContent = item.find(".content");
+
+        var height = item.height();
+        var width = item.width();
+        var left = item.position().left;
+        var top = item.position().top;
+
+        var border = item.css("border");
+        var boxShadow = item.css("box-shadow");
+
+        var contentBorder = itemContent.css("border");
+        var contentBoxShadow = itemContent.css("box-shadow");
+
+        itemContent.children().each(function(index, element){
+            $(element).css("visibility", "hidden");
+        });
+
+        item.css({
+            "border": "1px solid #fff",
+            "box-shadow": "none",
+        });
+
+        itemContent.css({
+            "border": "1px solid #fff",
+            "box-shadow": "none"
+        });
+
+        item.height(0)
+           .width(0)
+           .css("top", top + height/2)
+           .css("left", left + width/2);
+
+        setTimeout(function doHeaderAnimations(){
+
+            item.animate({
+                height: height,
+                width: width,
+                left: left,
+                top: top
+                
+            }, 500);
+            item.css({
+                opacity: 1
+            });
+        }, 1500 + extraDelay);
+
+        setTimeout(function(){
+            item.css({
+                "border": border,
+                "box-shadow": boxShadow
+            });
+            
+            itemContent.css({
+                "border": contentBorder,
+                "box-shadow": contentBoxShadow
+            });
+
+            itemContent.children().each(function(index, element){
+                $(element).css("visibility", "visible");
+            });
+
+            
+        }, 2000);
+
+    };
+
     setTimeout(animateHeaders, 500);
+    animateContentBoxes($("#readme"), 0);
+    animateContentBoxes($("#bandwidth"), 100);
+    animateContentBoxes($("#globalization"), 200);
     animateContainers();
 });
